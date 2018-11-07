6e801c50c4491c252adadcd03c78ec558969f1d0
Avin <carcinogen75@gmail.com>
2018-11-07 17:33:53 +0300

    fixes

--- src/NodeExpander/NodeExpander.js
+++ src/NodeExpander/NodeExpander.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const NodeExpander = ({ className, node, nodeDepth, nodeIndex, props }) => (
+const NodeExpander = ({ className, node, nodeDepth, nodeIndex, ...props }) => (
     <svg
         x="0px"
         y="0px"
--- src/NodeIcon/NodeIcon.js
+++ src/NodeIcon/NodeIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const NodeIcon = ({ className, node, nodeDepth, nodeIndex, props }) => (
+const NodeIcon = ({ className, node, nodeDepth, nodeIndex, ...props }) => (
     <svg
         x="0px"
         y="0px"
